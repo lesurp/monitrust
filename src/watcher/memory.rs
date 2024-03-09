b8c85878899039ebe2b678724eb006a8cd79d291
Paul Lesur <git@lesurpaul.fr>
2024-03-09 17:06:33 +0100

    fix memory reader

--- src/watcher/memory.rs
+++ src/watcher/memory.rs
@@ -1,9 +1,9 @@
+use std::io::{BufRead, BufReader};
 use std::time::Duration;
 
-use anyhow::{Context, Result};
-use nix::sys::sysinfo::sysinfo;
+use anyhow::{anyhow, Context, Result};
 use serde::{Deserialize, Serialize};
-use tracing::info;
+use tracing::{info, warn};
 
 use crate::watcher;
 use crate::watcher::ActiveAlert;
@@ -25,10 +25,57 @@ impl watcher::Checker for Checker {
 
     fn check(&self) -> Result<Self::CheckResult> {
         info!(checking = "memory");
-        let sysinfo = sysinfo().context("Could not execute 'sysinfo'")?;
-        let free_memory = (sysinfo.ram_unused() as f64) / (sysinfo.ram_total() as f64);
-        info!(free_memory);
-        Ok(free_memory)
+        let meminfo_file = "/proc/meminfo";
+        let file = std::fs::File::open(meminfo_file)
+            .context(format!("Could not open meminfo file at '{}'", meminfo_file))?;
+        let buf_read = BufReader::new(file);
+        const MEM_TOTAL_STR: &str = "MemTotal:";
+        const MEM_AVAIL_STR: &str = "MemAvailable:";
+        let mut mem_total = None;
+        let mut mem_avail = None;
+        for l in buf_read.lines() {
+            let l = l.context("Could not read line from meminfo file.")?;
+            let split = l.split_whitespace().collect::<Vec<_>>();
+            match &split[..] {
+                [name, value, _unit] => {
+                    let target = if *name == MEM_TOTAL_STR {
+                        &mut mem_total
+                    } else if *name == MEM_AVAIL_STR {
+                        &mut mem_avail
+                    } else {
+                        continue;
+                    };
+                    *target = Some(
+                        value
+                            .parse::<u64>()
+                            .context(format!("Could not convert memory to integer: {}", value))?,
+                    );
+                }
+                // Some values are just numbers e.g. page numbers
+                [_, _alue] => {}
+                _ => warn!("Could not parse line in meminfo file: {}", l),
+            }
+        }
+        match (mem_total, mem_avail) {
+            (Some(t), Some(a)) => {
+                let free_memory = (a as f64) / (t as f64);
+                info!(free_memory);
+                Ok(free_memory)
+            }
+            (Some(_), None) => Err(anyhow!(
+                "Could not retrieve available memory from meminfo file (key: {})",
+                MEM_AVAIL_STR
+            )),
+            (None, Some(_)) => Err(anyhow!(
+                "Could not retrieve total memory from meminfo file (key: {})",
+                MEM_TOTAL_STR
+            )),
+            (None, None) => Err(anyhow!(
+                "Could retrieve neither total nor available memory from meminfo file (keys: {} and {})",
+                MEM_TOTAL_STR, MEM_AVAIL_STR
+            )),
+        }
+        //let sysinfo = sysinfo().context("Could not execute 'sysinfo'")?;
     }
 
     fn period(&self) -> Duration {
